10d6d5983f54436f0a63f0bdc9c1a9373760e80d
Sindhu <sindhu.parajuli@mavs.uta.edu>
2020-01-27 19:38:45 -0600

    second commit

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    string str;
+    cout << "Enter Your Name Please: ";
+    getline(cin, str);
+    cout << "Hello, " << str << "!"<< endl;
+    return 0;
+}
+

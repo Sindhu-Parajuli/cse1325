300d051ec4b8a1b76fbcdc9ab1edd47839f27b2c
Sindhu <sindhu.parajuli@mavs.uta.edu>
2020-02-03 18:33:47 -0600

    Final commit

---
+++ P02/main.cpp
@@ -0,0 +1,23 @@
+#include "color.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+Color::Color(int red, int green, int blue):_red(red), _green(green), _blue(blue) {}
+
+
+std::string Color::to_string(){
+    return "(" + std::to_string(_red) + "," +  std::to_string(_green) +"," +  std::to_string(_blue) + ")";
+   
+}
+std::string Color::colorize (std::string text){
+ 
+  return  "\033[38;2;"+std::to_string(_red)+";"+std::to_string(_green)+";"+std::to_string(_blue)+"m"+text+"\033[0m";
+ // <<text
+ // <<"\033[0m"
+ // <<endl;
+ // return text;
+
+}
+

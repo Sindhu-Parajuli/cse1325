1b18a7ec35b81f6b6330082f0b582a37bb8794f3
Sindhu Parajuli <57790709+Sindhu-Parajuli@users.noreply.github.com>
2020-02-18 07:16:45 -0600

    Add files via upload

---
+++ coin.cpp
@@ -0,0 +1,14 @@
+#include "coin.h"
+#include <iostream>
+#include <string>
+
+Coin::Coin()
+{
+    
+}
+
+Coin::Coin(Coin_size size, Year year, char* notes):_size(size),_year(year),_notes{0}
+{
+  
+}
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <exception>
+#include "coin.h"
+
+
+int main()
+{
+    
+    Coin coin;
+	
+	
+    
+    
+    return 0;
+    
+    
+}
+
+
+
+
+
